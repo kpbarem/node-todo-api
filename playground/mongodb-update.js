cb32039913aa96cd259cc0127136b3a55246b5e2
Kevin Baremore <kevinbaremore@Kevins-MacBook-Pro.local>
2018-07-23 21:25:50 -0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b56a8f212bc39a68e387eab')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b569ed4838c414fc86305b0')
+  },
+{
+  $set :{
+    name: 'Kevin'
+  },
+  $inc: {
+    age: 1
+  }
+}, {
+  returnOriginal: false
+}).then((result) => {
+  console.log(result);
+})
+
+});
