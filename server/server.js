6b67c5799a6797f8d22aa99f3b8d92a56f149470
Kevin Baremore <kevinbaremore@Kevins-MacBook-Pro.local>
2018-07-28 19:55:34 -0700

    test the delete todos by id route

--- server/server.js
+++ server/server.js
@@ -57,8 +57,6 @@ app.delete('/todos/:id', (req, res) => {
 
   var id = req.params.id;
 
-
-
   //validate the id
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
@@ -69,7 +67,7 @@ app.delete('/todos/:id', (req, res) => {
     if(!todo) {
       return res.status(404).send();
     }
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
