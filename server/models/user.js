7ee66d728ba6072df8e75a3f2df23089c5ae7248
Kevin Baremore <kevinbaremore@Kevins-MacBook-Pro.local>
2018-07-30 20:54:02 -0700

    add delete users/me token route

--- server/models/user.js
+++ server/models/user.js
@@ -107,5 +107,17 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens:{
+        token
+      }
+    }
+  })
+}
+
 var User = mongoose.model('User', UserSchema);
 module.exports = {User};
